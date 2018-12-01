4bcb9f63fffbd4c453a4d956746cedeb7c9311d4
Marian Marian <fenrir55@gmail.com>
2018-12-01 17:08:39 +0100

    Zmiany nazw tabel, kolumn

--- EntityCodeFirst/Employee.cs
+++ EntityCodeFirst/Employee.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EntityCodeFirst
-{
+{[Table("tblEmployee")]
     public class Employee
     {
         public int Id { get; set; }
+        [Column("First_name")]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Gender { get; set; }
         public int Salary { get; set; }
+        public int DepartmentID { get; set; }
+        [ForeignKey("DepartmentID")]
         public Department Department { get; set; }
     }
 }
